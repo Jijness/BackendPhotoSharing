53c2f15a44ed4e9aa7551f54c228eb192aac2f08
B22DCCN881 Nguyễn Đăng Trường <122542661+Jijness@users.noreply.github.com>
2025-06-04 18:50:37 +0000

    4/6

--- routes/PhotoRouter.js
+++ routes/PhotoRouter.js
@@ -5,144 +5,157 @@ import { checkAuth } from "../middleware/checkAuth.js";
 
 import path from "path";
 import fs from "fs/promises";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
 const photoRoute = Router();
+// Hàm trợ giúp để lấy __dirname trong ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const imagesDir = path.join(__dirname, "..", "images");
 
 photoRoute.get("/", async (request, response) => {
-    const photos = await Photos.find({})
-        .select("_id user_id comments file_name date_time");
-    return response.status(200).json(photos);
+  const photos = await Photos.find({}).select("_id user_id comments file_name date_time");
+  return response.status(200).json(photos);
 });
 
 photoRoute.get("/:id", async (req, res) => {
-    try {
-        const userId = req.params.id;
-        // Check userId hop le
-        // if (!require("mongoose").Types.ObjectId.isValid(userId)) {
-        //     return response.status(400).json({message: "Invalid userId format"});
-        // }
-        const userExists = await Users.findById(userId);
-        if (!userExists) {
-            return res.status(404).json({ message: "User not found." });
-        }
-        const photos = await Photos.find({ user_id: userId })
-            .select("_id user_id comments file_name date_time")
-            .populate({
-                path: "comments.user_id",
-                select: "_id first_name last_name",
-            });
-
-        // Format lai dinh dang mang comments de chi chua cacc truong _id first_name last_name
-        const photosFormatted = photos.map((photo) => {
-            const photoObj = photo.toObject();
-
-            // Dinh dang lai mang comment chua user chu khong phai userId
-            photoObj.comments = photoObj.comments.map((comment) => {
-                const userInfo = {
-                    _id: comment.user_id._id,
-                    first_name: comment.user_id.first_name,
-                    last_name: comment.user_id.last_name,
-                };
-                return {
-                    _id: comment._id,
-                    comment: comment.comment,
-                    date_time: comment.date_time,
-                    user: userInfo, // tra ve thong tin truong user voi thong tin da format
-                };
-            });
-            return photoObj;
-        });
-        res.status(200).json(photosFormatted);
-    } catch (err) {
-        res.status(500).json({ message: "Error fetching photos of user", error: err.message });
+  try {
+    const userId = req.params.id;
+    // Check userId hop le
+    // if (!require("mongoose").Types.ObjectId.isValid(userId)) {
+    //     return response.status(400).json({message: "Invalid userId format"});
+    // }
+    const userExists = await Users.findById(userId);
+    if (!userExists) {
+      return res.status(404).json({ message: "User not found." });
     }
+    const photos = await Photos.find({ user_id: userId }).select("_id user_id comments file_name date_time").populate({
+      path: "comments.user_id",
+      select: "_id first_name last_name",
+    });
+
+    // Format lai dinh dang mang comments de chi chua cacc truong _id first_name last_name
+    const photosFormatted = photos.map((photo) => {
+      const photoObj = photo.toObject();
+
+      // Dinh dang lai mang comment chua user chu khong phai userId
+      photoObj.comments = photoObj.comments.map((comment) => {
+        const userInfo = {
+          _id: comment.user_id._id,
+          first_name: comment.user_id.first_name,
+          last_name: comment.user_id.last_name,
+        };
+        return {
+          _id: comment._id,
+          comment: comment.comment,
+          date_time: comment.date_time,
+          user: userInfo, // tra ve thong tin truong user voi thong tin da format
+        };
+      });
+      return photoObj;
+    });
+    res.status(200).json(photosFormatted);
+  } catch (err) {
+    res.status(500).json({ message: "Error fetching photos of user", error: err.message });
+  }
 });
 
 photoRoute.post("/commentsOfPhoto/:photo_id", checkAuth, async (req, res) => {
-    try {
-        const photoId = req.params.photo_id;
-        const { comment } = req.body;
-        const userId = req.user._id; // lay tu checkAuth
-        if (!comment) {
-            return res.status(400).json({ message: "Comment cannot be empty" });
-        }
-        const photo = await Photos.findById(photoId);
-        if (!photo) {
-            return res.status(404).json({ message: "Photo not found" });
-        }
-        photo.comments.push({
-            comment: comment,
-            user_id: userId,
-            date_time: new Date()
-        })
-        await photo.save();
-        // tim comment vua them vao
-        const resPhoto = await Photos.findById(photoId).select('comments')
-            .populate({
-                path: "comments.user_id",
-                select: "_id first_name last_name",
-            });
-        const newComment = resPhoto.comments.find(
-            c => c.comment === comment && c.user_id._id.toString() === userId.toString()
-        );
-        res.status(200).json({
-            message: "Comment added successfully",
-            newComment: newComment
-        });
-
-    } catch (err) {
-        console.error("Error adding comment:", err);
-        res.status(500).json({ message: "Error adding comment", error: err.message });
+  try {
+    const photoId = req.params.photo_id;
+    const { comment } = req.body;
+    const userId = req.user._id; // lay tu checkAuth
+    if (!comment) {
+      return res.status(400).json({ message: "Comment cannot be empty" });
     }
-})
+    const photo = await Photos.findById(photoId);
+    if (!photo) {
+      return res.status(404).json({ message: "Photo not found" });
+    }
+    photo.comments.push({
+      comment: comment,
+      user_id: userId,
+      date_time: new Date(),
+    });
+    await photo.save();
+    // tim comment vua them vao
+    const resPhoto = await Photos.findById(photoId).select("comments").populate({
+      path: "comments.user_id",
+      select: "_id first_name last_name",
+    });
+    const newComment = resPhoto.comments.find((c) => c.comment === comment && c.user_id._id.toString() === userId.toString());
+    res.status(200).json({
+      message: "Comment added successfully",
+      newComment: newComment,
+    });
+  } catch (err) {
+    console.error("Error adding comment:", err);
+    res.status(500).json({ message: "Error adding comment", error: err.message });
+  }
+});
 
 photoRoute.post("/new", checkAuth, async (req, res) => {
-    try {
-
-        if (!req.files || Object.keys(req.files).length === 0) {
-            return res.status(400).json({ message: "No file uploaded" });
-        }
-        const photoFile = req.files.photo;
-
-        const allowedTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/webp'];
-        if (!allowedTypes.includes(photoFile.mimetype)) {
-            return res.status(400).json({ message: "Invalid file type. Only JPEG, PNG, GIF and WebP are allowed." });
-        }
-        // Tao ten file duy nhat cho anh moi
-        const ogirinalFileName = photoFile.name;
-        const fileExtension = path.extname(ogirinalFileName);
-        const uniqueFileName = `${Date.now()}-${Math.random() * 1E9}${fileExtension}`;
-        // Duong dan de luu file tren server
-        const __filename = fileURLToPath(import.meta.url);
-        const __dirname = path.dirname(__filename);
-        const imagesDir = path.join(__dirname, '..', 'images');
-        await fs.mkdir(imagesDir, { recursive: true });
-
-        const uploadPath = path.join(imagesDir, uniqueFileName);
-        // Chuyen file da upload vao thu muc images
-        await photoFile.mv(uploadPath);
-
-        const userId = req.user._id; // tu checkAuth
-
-        const newPhoto = new Photos({
-            file_name: uniqueFileName,
-            date_time: new Date(),
-            user_id: userId,
-            comments: []
-        })
-        await newPhoto.save();
-
-        res.status(201).json({ message: "Photo uploaded successfully" });
-
-    } catch (err) {
-        console.error("Error uploading photo:", err);
-        if (err.code === 'ENOENT') { // Lỗi thư mục không tồn tại
-            res.status(500).json({ message: "Server error: Image upload directory not found or created.", error: err.message });
-        } else {
-            res.status(500).json({ message: "Error uploading photo", error: err.message });
-        }
+  try {
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).json({ message: "No file uploaded" });
     }
-})
+    const photoFile = req.files.photo;
+
+    const allowedTypes = ["image/jpeg", "image/png", "image/gif", "image/webp"];
+    if (!allowedTypes.includes(photoFile.mimetype)) {
+      return res.status(400).json({
+        message: "Invalid file type. Only JPEG, PNG, GIF and WebP are allowed.",
+      });
+    }
+    // Tao ten file duy nhat cho anh moi
+    const ogirinalFileName = photoFile.name;
+    const fileExtension = path.extname(ogirinalFileName);
+    const uniqueFileName = `${Date.now()}-${Math.random() * 1e9}${fileExtension}`;
+    // Duong dan de luu file tren server
+    // const __filename = fileURLToPath(import.meta.url);
+    // const __dirname = path.dirname(__filename);
+    // const imagesDir = path.join(__dirname, "..", "images");
+    await fs.mkdir(imagesDir, { recursive: true });
+
+    const uploadPath = path.join(imagesDir, uniqueFileName);
+    // Chuyen file da upload vao thu muc images
+    await photoFile.mv(uploadPath);
+
+    const userId = req.user._id; // tu checkAuth
+
+    const newPhoto = new Photos({
+      file_name: uniqueFileName,
+      date_time: new Date(),
+      user_id: userId,
+      comments: [],
+    });
+    await newPhoto.save();
+
+    res.status(201).json({ message: "Photo uploaded successfully" });
+  } catch (err) {
+    console.error("Error uploading photo:", err);
+    if (err.code === "ENOENT") {
+      // Lỗi thư mục không tồn tại
+      res.status(500).json({
+        message: "Server error: Image upload directory not found or created.",
+        error: err.message,
+      });
+    } else {
+      res.status(500).json({ message: "Error uploading photo", error: err.message });
+    }
+  }
+});
+
+photoRoute.delete("/:photoId", checkAuth, async (req, res) => {
+  try {
+    // check photo.user_id khop voi user._id
+    // await Photos.deleteOne({ _id: photoId });
+    // // Xoa anh that tren server
+    // const filePath = path.join(imagesDir, photo.file_name);
+    // await fs.unlink(filePath);
+  } catch (err) {
+    res.status(500).json({ message: "Error while deleting photo", error: err.message });
+  }
+});
 
 export default photoRoute;
